e9cadccd6b4b25145e68cf717a26a3997be7150d
Vincent Neo <76011299@qq.com>
2023-03-22 11:30:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 // Routes
 app.use("/home", home);
 
+app.get('/', (req, res) => {
+  res.send('欢迎来到我的网站！');
+});
+
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
